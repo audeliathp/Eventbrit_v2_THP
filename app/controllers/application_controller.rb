58903693c86b0245ff13e525c853537aa4aebd76
Julien Lamalle <eparis@atlassian.com>
2020-08-10 21:35:16 +0200

    edit sign out redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
 
 
 
+  private 
+  
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
   protected
 
   def configure_permitted_parameters
