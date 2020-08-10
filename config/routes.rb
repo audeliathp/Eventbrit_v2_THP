1f61f36074bc0cb907fd2515b3546069c13c32fa
Julien Lamalle <eparis@atlassian.com>
2020-08-10 18:03:37 +0200

    update with precomplile assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users
 
   root 'events#index'
 
   resources :events, only: [:index, :new]
   resources :users,  only: [:show]
-=======
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> 05010483cda7526306d0870e70c01e554b5c15da
 end
