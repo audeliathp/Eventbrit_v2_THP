0e0474b15a1ea06175685ad78d769567d7a6e5ea
Julien Lamalle <eparis@atlassian.com>
2020-08-11 15:07:31 +0200

    create event show view, update users show view and index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
-  def index
+  before_action :authenticate_user!, only: [:show]
+  before_action :is_owner?, only: [:show]
+  
+  def show
     @user = current_user
     @events = Event.where(admin_id: current_user.id)
+  end
+
+  private 
+
+  def is_owner?
+    if current_user.id.to_i != params[:id].to_i
+      flash[:danger] = "Vous ne pouvez pas acceder à cette page"
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'events#index'
 
-  resources :events, only: [:index, :new]
-  resources :users,  only: [:index, :show]
+  resources :events, only: [:index, :new, :show]
+  resources :users,  only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
